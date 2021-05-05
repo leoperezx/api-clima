acede82ed3585ee125433fb6bde7caadf393596e
leoperezx <leoperez.x@gmail.com>
2021-05-04 22:42:27 -0500

    issue 3 done

--- public/src/clock.js
+++ public/src/clock.js
@@ -1,39 +1,34 @@
-// hora del sistema
+function getmonth() {
+  const date = new Date();
 
-
-function mouthBynumber(mes){
-    let mounthText
-    if (mes == 0) {mounthText = "enero"}
-    if (mes == 1) {mounthText = "febrero"}
-    if (mes == 2) {mounthText = "marzo"}
-    if (mes == 3) {mounthText = "abril"}
-    if (mes == 4) {mounthText = "mayo"}
-    if (mes == 5) {mounthText = "junio"}
-    if (mes == 6) {mounthText = "julio"}
-    if (mes == 7) {mounthText = "agosto"}
-    if (mes == 8) {mounthText = "setpiembre"}
-    if (mes == 9) {mounthText = "octubre"}
-    if (mes == 10) {mounthText = "noviembre"}
-    if (mes == 11) {mounthText = "diciembre"}
-    return mounthText
+  const hora = date.getHours();
+  const dia = date.getDate();
+  const mes = date.toLocaleString("es-co", { month: "long" });
+  const ano = date.getFullYear();
+  return { hora, dia, mes, ano };
 }
 
-function saludo(){
-    const hora = new Date().getHours()
-    const dia = new Date().getDate()
-    const mes = new Date().getMonth()
-    const ano = new Date().getFullYear()
+function saludo() {
+  const info = getmonth();
 
-    let mensajeDiaTardeNoche
+  let mensajeDiaTardeNoche;
 
-    if (hora < 13) {mensajeDiaTardeNoche = "Buenos días"; styleName ="mañana"}
-    else if (hora < 19) {mensajeDiaTardeNoche = "Buenas tardes"; styleName ="tarde"}
-    else if (hora < 24) {mensajeDiaTardeNoche = "Buenas noches"; styleName ="noche"}
-    
-    document.getElementById("greetings").innerHTML = mensajeDiaTardeNoche
-    document.getElementById("greetingWithDate").innerHTML = ("Hoy es " + dia + " de " + mouthBynumber(mes) + " de " + ano)
-    document.body.className = styleName
+  if (info.hora < 13) {
+    mensajeDiaTardeNoche = "Buenos días";
+    styleName = "mañana";
+  } else if (info.hora < 19) {
+    mensajeDiaTardeNoche = "Buenas tardes";
+    styleName = "tarde";
+  } else if (info.hora < 24) {
+    mensajeDiaTardeNoche = "Buenas noches";
+    styleName = "noche";
+  }
+
+  document.querySelector(".greetings").innerText = mensajeDiaTardeNoche;
+  document.querySelector(
+    ".greetingWithDate"
+  ).innerText = `Hoy es ${info.dia} de ${info.mes} de ${info.ano}`;
+  document.body.className = styleName;
 }
 
-
-saludo()
+saludo();
